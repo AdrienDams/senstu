f9961c3a4ba62dce8eb4358138eefc94c3afff2b
AdrienDams <adriendamseaux@hotmail.fr>
2023-06-21 16:31:34 +0200

    add closest grid script

---
+++ time_series/closest_grid.py
@@ -0,0 +1,34 @@
+import netCDF4 as nc
+import numpy as np
+
+# Open the NetCDF file
+file_path = "57_DOM02_001.clm2.h0.2022-01-01-00000.nc"
+dataset = nc.Dataset(file_path, 'r')
+
+# Read latitude and longitude variables
+lat = dataset.variables['lat'][:]
+lon = dataset.variables['lon'][:]
+
+# Target coordinates
+target_lat = 72.369555
+target_lon = 126.475111
+
+# Calculate the distance between target coordinates and each pixel
+lat_diff = lat - target_lat
+lon_diff = lon - target_lon
+distance = np.sqrt(lat_diff ** 2 + lon_diff ** 2)
+
+# Find the index of the closest pixel
+closest_index = np.argmin(distance)
+
+# Get the latitude and longitude values of the closest pixel
+closest_lat = lat[closest_index]
+closest_lon = lon[closest_index]
+
+# Close the NetCDF file
+dataset.close()
+
+# Print the index and coordinates of the closest pixel
+print("Closest Pixel Index:", closest_index)
+print("Closest Pixel Latitude:", closest_lat)
+print("Closest Pixel Longitude:", closest_lon)
